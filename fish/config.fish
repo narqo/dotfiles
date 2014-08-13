dc79736775eb10b555f0a2d23fa9e3e6ec89f1b0
Vladimir Varankin <nek.narqo+git@gmail.com>
2014-08-14 03:18:42 +0400

    tune fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,24 @@
-if status --is-interactive
-    set PATH $PATH ~/bin ~/.local/bin ./node_modules/.bin
-    set LANG=ru_RU.UTF-8
+# vim: ft=conf
+
+# Some local XDG-like dirs
+set -l confdir ~/.config
+set -l datadir ~/.local/share
+
+# Set up function paths
+set fish_function_path $confdir/fish/functions $datadir/fish/functions $__fish_sysconfdir/functions $__fish_datadir/functions
+
+# Clean up default fish greeting
+set -gx fish_greeting ''
+
+# Set up colors
+set -g fish_color_command ffd7ff
+set -g fish_color_param ffd7ff
+
+if status --is-login
+    set -gx LANG ru_RU.UTF-8
 end
 
-set -g -x fish_greeting ''
-
-# colors
-set -U fish_color_command ffd7ff
-set -U fish_color_param ffd7ff
+if status --is-interactive
+    set EDITOR vim
+    set PATH $PATH ~/bin ~/.local/bin ./node_modules/.bin
+end
