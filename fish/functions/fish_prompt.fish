ed1a280a3d7079f40d679f18dbbe5aa7af80bd1a
Vladimir Varankin <nek.narqo+git@gmail.com>
2014-08-17 13:51:12 +0400

    fix fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,26 +1,30 @@
 # fish git prompt
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_showdirtystate "yes"
+set __fish_git_prompt_showupstream "yes"
 set __fish_git_prompt_color_branch purple
 
 # Status Chars
-set __fish_git_prompt_char_dirtystate '±'
-set __fish_git_prompt_char_cleanstate ''
-set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_upstream_ahead '↑'
-set __fish_git_prompt_char_upstream_behind '↓'
+set __fish_git_prompt_char_dirtystate "±"
+set __fish_git_prompt_char_cleanstate ""
+set __fish_git_prompt_char_stagedstate "→"
+set __fish_git_prompt_char_invalidstate "✖"
+set __fish_git_prompt_char_upstream_ahead "↑"
+set __fish_git_prompt_char_upstream_behind "↓"
+set __fish_git_prompt_char_upstream_equal ""
+set __fish_git_prompt_char_untrackedfiles "…"
 
 function fish_prompt
    set_color red
-   printf '%s' (hostname|cut -d . -f 1)
+   printf "%s" (hostname|cut -d . -f 1)
    set_color normal
-   printf ' £ '
+   printf " £ "
 
    set_color $fish_color_cwd
-   printf '%s' (prompt_pwd)
+   printf "%s" (prompt_pwd)
 
    set_color normal
-   printf '%s' (__fish_git_prompt)
+   set -l git_prompt_format " ⤳  %s"
+   printf "%s" (__fish_git_prompt $git_prompt_format)
 
    set_color normal
 
@@ -29,6 +33,6 @@ function fish_prompt
    if test $VIRTUAL_ENV
        printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
    end
-   printf '  › '
+   printf "  › "
    set_color normal
 end
