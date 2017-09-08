07d56faefa13dbe64a0976e11bfbad1b01dce8cf
Vladimir Varankin <nek.narqo+git@gmail.com>
2017-09-08 13:01:39 +0300

    fix goget alias

--- fish/functions/goget.fish
+++ fish/functions/goget.fish
@@ -1,5 +1,5 @@
 function goget --description 'Get Go package'
-    set -l __gopath (go env GOPATH | awk -F":" "{print $FN}")
+    set -l __gopath (go env GOPATH | awk -F: '{print $NF}')
     command env GOPATH=$__gopath go get -u -d -v $argv
 end
 
