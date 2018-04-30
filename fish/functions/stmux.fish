b53734d7d83ee231179b2da3cfa7df3d25454cfc
Vladimir Varankin <vladimir@varank.in>
2018-04-30 18:34:15 +0200

    fish: add --wraps to custom function definitions
    wraps cause function to inherit completions from the wrapped command

--- fish/functions/stmux.fish
+++ fish/functions/stmux.fish
@@ -1,3 +1,3 @@
-function stmux --description 'SSH to tmux session'
+function stmux --wraps ssh --description 'SSH to tmux session'
     command ssh -t $argv 'tmux -CC -u attach-session || tmux -CC -u new-session'
 end
--- fish/functions/svim.fish
+++ fish/functions/svim.fish
@@ -1,3 +1,3 @@
-function svim --description 'Vi IMproved, a programmers text editor (sudo)'
+function svim --wraps vim --description 'Vi IMproved, a programmers text editor (sudo)'
     command sudo vim $argv
 end
