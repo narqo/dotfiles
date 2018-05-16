8fa268b922adb351c2f1a0ed8213ba83c683f856
Vladimir Varankin <vladimir@varank.in>
2018-05-16 14:23:55 +0200

    force stmux to run with 256 colours

--- fish/functions/stmux.fish
+++ fish/functions/stmux.fish
@@ -1,3 +1,3 @@
 function stmux --wraps ssh --description 'SSH to tmux session'
-    command ssh -t $argv 'tmux -CC -u attach-session || tmux -CC -u new-session'
+    command ssh -t $argv 'tmux -CC -2 -u attach-session || tmux -CC -2 -u new-session'
 end
