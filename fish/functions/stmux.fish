0ae73ba13b2e1812ff22c0d26a27f9b6e51d56e2
Vladimir Varankin <vladimir@varank.in>
2018-09-07 14:22:47 +0200

    update fish tmux alias to be nicier to iterm-s tmux integration

--- fish/functions/stmux.fish
+++ fish/functions/stmux.fish
@@ -1,3 +1,3 @@
 function stmux --wraps ssh --description 'SSH to tmux session'
-    command ssh -t $argv 'tmux -CC -2 -u attach-session || tmux -CC -2 -u new-session'
+    command ssh -t $argv -- 'tmux has-session && tmux -CC -2 -u attach-session || tmux -CC -2 -u new-session'
 end
