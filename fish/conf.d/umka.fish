da5f5454dc5c0920533b6fff187b8d7a6550781d
Vladimir Varankin <vladimir@varank.in>
2021-04-13 16:36:50 +0200

    fish: tune umka colors

--- fish/conf.d/umka.fish
+++ fish/conf.d/umka.fish
@@ -1,9 +1,11 @@
 set -g fish_color_normal normal
 set -g fish_color_command normal
 set -g fish_color_quote normal
-set -g fish_color_redirection cyan
+set -g fish_color_redirection brblue
 set -g fish_color_end 767676
 set -g fish_color_error red --bold
+set -g fish_color_param normal
+set -g fish_color_autosuggestion brcyan
 set -g fish_color_selection white --bold --background=cyan
 set -g fish_color_search_match bryellow --background=cyan
 set -g fish_color_history_current --bold
@@ -12,14 +14,14 @@ set -g fish_color_escape 00a6b2
 set -g fish_color_cwd green
 set -g fish_color_cwd_root red
 set -g fish_color_valid_path --underline
-set -g fish_color_autosuggestion normal
 set -g fish_color_user brgreen
 set -g fish_color_host normal
 set -g fish_color_cancel normal
+set -g fish_color_keyword normal --bold
+set -g fish_color_comment brblack
+set -g fish_color_match --background=cyan
+
 set -g fish_pager_color_completion normal
 set -g fish_pager_color_description B3A06D yellow
 set -g fish_pager_color_prefix normal --bold --underline
 set -g fish_pager_color_progress brwhite --background=cyan
-set -g fish_color_param 303030
-set -g fish_color_comment 303030
-set -g fish_color_match --background=cyan
