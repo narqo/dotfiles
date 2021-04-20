21c6af1c5e1369e12d309041cc545104250c1a46
Vladimir Varankin <vladimir@varank.in>
2021-04-20 09:09:30 +0200

    fish: tune umka

--- fish/conf.d/umka.fish
+++ fish/conf.d/umka.fish
@@ -5,7 +5,7 @@ set -g fish_color_redirection brblue
 set -g fish_color_end 767676
 set -g fish_color_error red --bold
 set -g fish_color_param normal
-set -g fish_color_autosuggestion brcyan
+set -g fish_color_autosuggestion brblack
 set -g fish_color_selection white --bold --background=cyan
 set -g fish_color_search_match bryellow --background=cyan
 set -g fish_color_history_current --bold
